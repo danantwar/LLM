fa432c3b41a45e87d8db203e8dd40373ec0daa8d
danantwar <dnyaneshwar.anantwar@fusiongbs.com>
2023-08-28 20:40:23 +0530

    FileLoad Code

---
+++ Flask/fileLoad.py
@@ -0,0 +1,27 @@
+# Import the necessary Python libraries
+from flask import request
+from werkzeug.utils import secure_filename
+import webLoadFunctions as wb
+
+def upload():
+    
+    #Initialize Variables
+    source = "FILE"
+    
+    # Get the file from the request
+    file = request.files['file']
+
+    # Save the file to a temporary location
+    filename = secure_filename(file.filename)
+  
+#   file.save(os.path.join(app.config['C:\\PythonProjects\\LLM\\Flask\\temp'], filename))
+
+    # Load the contents of the file into a string
+    file_content = file.read().decode('utf-8')
+
+    # Add enty in history table for this file
+    curLoadTimeStamp = wb.addLoadHistoryInDB(source)
+
+    # Load content in database
+    wb.loadDataInDB (source, filename, file_content)
+    
